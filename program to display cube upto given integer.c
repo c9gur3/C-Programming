3757ce56641e9111501606e01430e1e2face74ef
c9gur3 <102017496+c9gur3@users.noreply.github.com>
2022-04-18 16:27:05 +0300

    Add files via upload

---
+++ program to display cube upto given integer.c
@@ -0,0 +1,14 @@
+//program to print the cube of 1 to 5
+#include<stdio.h>
+
+int main()
+{
+    int i,ctr;
+    printf("input number of terms");
+    scanf("%d",&ctr);
+    for(i=1;i<=ctr; i++)
+    {
+    printf("number is :%d and the cube of the %d is :%d\n",i,i,(i*i*i));
+    }
+    return 0;
+}
